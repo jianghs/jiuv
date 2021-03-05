9d469460f33dc19ff5288acf181545bf9a48bec3
jianghs430 <jianghs430@zto.com>
2021-03-05 17:42:54 +0800

    add app modules

---
+++ jiuv-app/src/main/java/me/jianghs/jiuv/app/JiuvAppApplication.java
@@ -0,0 +1,13 @@
+package me.jianghs.jiuv.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiuvAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiuvAppApplication.class, args);
+    }
+
+}
---
+++ jiuv-app/src/test/java/me/jianghs/jiuv/app/JiuvAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jianghs.jiuv.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiuvAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

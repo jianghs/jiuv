0bc79ee502bec67a34b6d5433f26e0609059d353
jianghs430 <jianghs430@zto.com>
2021-03-05 17:06:23 +0800

    init

---
+++ jiuv-domain/src/main/java/me/jianghs/jiuv/domain/test/Test.java
@@ -0,0 +1,19 @@
+package me.jianghs.jiuv.domain.test;
+
+/**
+ * @className: Test
+ * @description:
+ * @author: jianghs430
+ * @createDate: 2021/3/5 16:11
+ * @version: 1.0
+ */
+public class Test {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+}
---
+++ jiuv-domain/src/test/java/me/jianghs/jiuv/domain/test/TestTest.java
@@ -0,0 +1,18 @@
+package me.jianghs.jiuv.domain.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TestTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void sub() {
+    }
+}

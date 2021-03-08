381fa1441e804f63e92581c5e6cbf15e169154f5
jianghs430 <jianghs430@zto.com>
2021-03-08 15:52:27 +0800

    add other modules

---
+++ jiuv-controller/src/test/java/me/jianghs/jiuv/controller/JiuvControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jianghs.jiuv.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiuvControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ jiuv-infrastructure/src/main/java/me/jianghs/jiuv/infrastructure/infrastructure/JiuvInfrastructureApplication.java
@@ -0,0 +1,13 @@
+package me.jianghs.jiuv.infrastructure.infrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiuvInfrastructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiuvInfrastructureApplication.class, args);
+    }
+
+}
